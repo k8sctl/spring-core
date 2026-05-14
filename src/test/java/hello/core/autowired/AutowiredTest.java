5be585bdb1bd5c79a920530dea09891753ed4862
thbins <joons0218@gmail.com>
2026-05-14 21:04:12 +0900

    feat: 빈 생명주기 콜백 적용을 위한 기본 예제 세팅

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
 
 import java.util.Optional;
