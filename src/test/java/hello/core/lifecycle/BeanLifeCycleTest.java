652e3fcd99a21754ba740e90f59137dc81719485
thbins <joons0218@gmail.com>
2026-05-14 23:28:54 +0900

    feat: @Bean 속성 방식으로 빈 생명주기 콜백 구현
    - initMethod = init: 초기화 콜백 지정
    - destroyMethod = close: 소멸 콜백 지정
    - destroyMethod 기본값(inferred)으로 close/shutdown 자동 추론 가능

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,12 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        // destroyMethod의 디폴트 값은 (inferred)로 되어있다.
+        // 이는 '추론'이라는 의미로,
+        // 라이브러리는 대부분 close, shutdown으로 되어있기 때문에
+        // 이러한 메서드들을 자동으로 호출해준다.
+        // 추론 기능을 사용하고 싶지 않다면 빈 문자열을 입력하면 된다. (예시: destroyMethod = "")
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
